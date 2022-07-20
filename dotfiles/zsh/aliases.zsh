baffb03c309144c42328f64d21aa2fc6af2bc046
Roee Shapira <ro33.sha@gmail.com>
2022-07-20 20:10:06 +0300

    Remove unneeded asliases

--- dotfiles/zsh/aliases.zsh
+++ dotfiles/zsh/aliases.zsh
@@ -9,11 +9,9 @@ if (( $+commands[exa] )); then
 fi
 alias l='ls -lbF' # list, size, type
 alias ll='ls -la' # long, all
-alias llm='ll --sort=modified' # list, long, sort by modification date
 alias la='ls -lbhHigUmuSa' # all list
 alias lx='ls -lbhHigUmuSa@' # all list and extended
 alias tree='exa --tree' # tree view
-alias lS='exa -1' # one column by just names
 
 # Neovim
 alias v='nvim'
