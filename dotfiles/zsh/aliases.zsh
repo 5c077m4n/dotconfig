e090b7267991c9634b14c9f378d75f77d188ae33
Roee Shapira <ro33.sha@gmail.com>
2022-07-20 20:10:59 +0300

    fixup! Remove unneeded asliases

--- dotfiles/zsh/aliases.zsh
+++ dotfiles/zsh/aliases.zsh
@@ -6,12 +6,12 @@ alias cdt='cd "$(mktemp -d)"'
 # exa -> ls
 if (( $+commands[exa] )); then
 	alias ls='exa' # just replace ls by exa and allow all other exa arguments
+	alias tree='exa --tree' # tree view
 fi
 alias l='ls -lbF' # list, size, type
 alias ll='ls -la' # long, all
 alias la='ls -lbhHigUmuSa' # all list
 alias lx='ls -lbhHigUmuSa@' # all list and extended
-alias tree='exa --tree' # tree view
 
 # Neovim
 alias v='nvim'
