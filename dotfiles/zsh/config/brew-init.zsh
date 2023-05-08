d081baa8406167444200adfe71b19ed3e2c8b9a5
Roee Shapira <ro33.sha@gmail.com>
2023-05-08 16:46:40 +0300

    Simplify brew init

--- dotfiles/zsh/config/brew-init.zsh
+++ dotfiles/zsh/config/brew-init.zsh
@@ -1,26 +1,20 @@
-() {
-	if (( $+commands[brew] )); then
-		export HOMEBREW_NO_ANALYTICS=1
-		export HOMEBREW_PREFIX="$(brew --prefix)"
-		export HOMEBREW_BUNDLE_FILE="${HOME}/repos/dotconfig/assets/macos/Brewfile"
+if (( $+commands[brew] )); then
+	export HOMEBREW_NO_ANALYTICS=1
+	export HOMEBREW_PREFIX="$(brew --prefix)"
+	export HOMEBREW_BUNDLE_FILE="${HOME}/repos/dotconfig/assets/macos/Brewfile"
 
-		eval "$(brew shellenv)"
+	eval "$(brew shellenv)"
 
-		path+=("/opt/homebrew/bin")
-		fpath+=("${HOMEBREW_PREFIX}/share/zsh/site-functions")
-
-		# Use GNU tools as default
-		local gnu_bin_dir
-		for gnu_bin_dir in ${HOMEBREW_PREFIX}/opt/*/libexec/gnubin; do
-			[[ -d "$gnu_bin_dir" ]] && path+=("$gnu_bin_dir")
-		done
-
-		if [[ -d "${HOMEBREW_PREFIX}/Caskroom/google-cloud-sdk" ]]; then
-			# Gcloud init
-			export USE_GKE_GCLOUD_AUTH_PLUGIN=True
-			export GOOGLE_APPLICATION_CREDENTIALS="${XDG_CONFIG_HOME}/gcloud/application_default_credentials.json"
-			source "${HOMEBREW_PREFIX}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
-			source "${HOMEBREW_PREFIX}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
-		fi
+	if [[ -d "${HOMEBREW_PREFIX}/Caskroom/google-cloud-sdk" ]]; then
+		# Gcloud init
+		export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+		export GOOGLE_APPLICATION_CREDENTIALS="${XDG_CONFIG_HOME}/gcloud/application_default_credentials.json"
+		source "${HOMEBREW_PREFIX}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+		source "${HOMEBREW_PREFIX}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
 	fi
-}
+
+	fpath+=("${HOMEBREW_PREFIX}/share/zsh/site-functions")
+
+	# Use GNU tools as default
+	path=("${HOMEBREW_PREFIX}/opt/coreutils/libexec/gnubin" $path)
+fi
