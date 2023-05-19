66ae9900ea3d095db73f28d9f4ba2b7d13a4817d
Roee Shapira <ro33.sha@gmail.com>
2023-05-19 20:31:43 +0300

    Add rust cargo init

--- dotfiles/zsh/config/005.path.zsh
+++ dotfiles/zsh/config/005.path.zsh
@@ -1,6 +1,7 @@
 if [[ -d "${KREW_ROOT:-$HOME/.krew}/bin" ]]; then
 	path+=("${KREW_ROOT:-$HOME/.krew}/bin")
 fi
+[[ -f "${HOME}/.cargo/env" ]] && source "${HOME}/.cargo/env"
 
 path+=("${HOME}/.local/bin" "${GOBIN}")
 cdpath+=("${HOME}/repos")
