4f53a47332349124b6937a0cd2bb042d284fdb75
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-09-01 23:58:57 +0300

    Change the user.js creation script to an anon fn

--- assets/firefox/gen-user-js.zsh
+++ assets/firefox/gen-user-js.zsh
@@ -1,7 +1,9 @@
 #!/usr/bin/env zsh
 
-arkenfox_user="$(curl https://raw.githubusercontent.com/arkenfox/user.js/master/user.js)"
-__dirname="${0:A:h}"
-ff_profile_dir="${1:-"."}"
+() {
+	local arkenfox_user="$(curl https://raw.githubusercontent.com/arkenfox/user.js/master/user.js)"
+	local __dirname="$1"
+	local ff_profile_dir="${2:-"."}"
 
-{ echo $arkenfox_user; echo "\n"; cat "${__dirname}/user-overrides.js"; } > "${ff_profile_dir}/user.js"
+	{ echo $arkenfox_user; echo "\n"; cat "${__dirname}/user-overrides.js"; } > "${ff_profile_dir}/user.js"
+} "${0:A:h}" "$@"
