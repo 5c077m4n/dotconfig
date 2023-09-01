35b0e09ba6ed19c362e5bc0ca7d6e3d39f00e786
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-09-01 23:59:32 +0300

    Replace zap w/ omz

--- dotfiles/zsh/config/upsert-pkgs.zsh
+++ dotfiles/zsh/config/upsert-pkgs.zsh
@@ -5,3 +5,7 @@
 		git clone https://github.com/tmux-plugins/tpm "$tpm_path"
 	fi
 }
+
+if [[ ! -f "${ZSH}/oh-my-zsh.sh" ]]; then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
