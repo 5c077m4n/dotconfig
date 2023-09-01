9b17b7e5f91c26c847656612bff3774523a6bc4f
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-09-01 23:59:36 +0300

    Add SIGUSR1 listener to reload nvim config

---
+++ dotfiles/nvim/lua/vimrc/utils/signals.lua
@@ -0,0 +1,33 @@
+local reload_vimrc = require('vimrc.utils.modules').reload_vimrc
+
+local uv, split, trim, system = vim.loop, vim.split, vim.trim, vim.fn.system
+local USR1_SIGNAL = 'sigusr1'
+
+local function handle_usr1()
+	---@diagnostic disable-next-line: undefined-field
+	local signal_handler = uv.new_signal()
+	---@diagnostic disable-next-line: undefined-field
+	uv.signal_start_oneshot(signal_handler, USR1_SIGNAL, function(signal_usr1)
+		vim.notify('Received a "' .. string.upper(signal_usr1) .. '" signal, reloading...')
+		vim.schedule(reload_vimrc)
+	end)
+end
+
+local function send_usr1_to_all_nvim()
+	local nvim_pids = split(trim(system({ 'pgrep', 'nvim' })), '\n')
+
+	for _, pid_str in ipairs(nvim_pids) do
+		local pid = tonumber(pid_str)
+
+		if type(pid) == 'number' then
+			vim.notify('Sending a reload signal to NVIM instance (PID ' .. pid .. ')')
+			---@diagnostic disable-next-line: undefined-field
+			uv.kill(pid, USR1_SIGNAL)
+		end
+	end
+end
+
+return {
+	handle_usr1 = handle_usr1,
+	send_usr1_to_all_nvim = send_usr1_to_all_nvim,
+}
