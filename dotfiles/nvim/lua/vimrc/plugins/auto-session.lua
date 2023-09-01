94c05cd4d351cf3b8b99386a87227c63c2a8dda1
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-09-01 23:59:19 +0300

    Remove auto session plugin

--- dotfiles/nvim/lua/vimrc/plugins/auto-session.lua
+++
@@ -1,7 +0,0 @@
-require('auto-session').setup({
-	auto_session_enable_last_session = true,
-	auto_session_root_dir = vim.fn.stdpath('data') .. '/sessions/',
-	auto_session_enabled = true,
-	auto_save_enabled = true,
-	auto_restore_enabled = true,
-})
