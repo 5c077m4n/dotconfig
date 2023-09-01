f1b482c4b06ab68fbd0f2a8e6e6c05965a8c610c
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-09-01 23:59:19 +0300

    Add typing

--- dotfiles/nvim/lua/vimrc/utils/keymapping.lua
+++ dotfiles/nvim/lua/vimrc/utils/keymapping.lua
@@ -1,11 +1,15 @@
 local M = {}
 
+---@param mode string|table
 local function create_keymap_fn(mode)
 	mode = mode or 'n'
 
-	return function(lhs, rhs, options)
+	---@param buttons string
+	---@param command string|function
+	---@param options table|nil
+	return function(buttons, command, options)
 		local opts = vim.tbl_extend('force', { noremap = true, silent = true }, options or {})
-		vim.keymap.set(mode, lhs, rhs, opts)
+		vim.keymap.set(mode, buttons, command, opts)
 	end
 end
 
