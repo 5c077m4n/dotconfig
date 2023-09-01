6b84d073a756e70984dac71af8d923d69d492ddc
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-09-01 23:59:34 +0300

    Allow autopairs plugin without cmp

--- dotfiles/nvim/lua/vimrc/plugins/init.lua
+++ dotfiles/nvim/lua/vimrc/plugins/init.lua
@@ -349,10 +349,11 @@ function M.setup()
 				config = function()
 					require('nvim-autopairs').setup({ check_ts = true })
 
-					local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-					local cmp = require('cmp')
-
-					cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+					local ok, cmp = pcall(require, 'cmp')
+					if ok then
+						local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+						cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+					end
 				end,
 			})
 			use({
