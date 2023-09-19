edebbbb9a0c371eef850a166f2a0a0b5af0e1483
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-09-19 14:42:13 +0300

    Replace syntax highlight omz plugin with original

--- dotfiles/zsh/config/003.omz-plugins.zsh
+++ dotfiles/zsh/config/003.omz-plugins.zsh
@@ -3,13 +3,13 @@ plugins=(fzf rust)
 
 () {
 	local plugins_custom=(
-		zdharma-continuum/fast-syntax-highlighting
 		hlissner/zsh-autopair
 		zsh-users/zsh-autosuggestions
 		zsh-users/zsh-completions
+		zsh-users/zsh-syntax-highlighting
 	)
-	local plug_repo
 
+	local plug_repo
 	for plug_repo in $plugins_custom; do
 		local plug_name="$(echo "$plug_repo" | sed 's|.*\/||')"
 		local plug_dir="${ZSH_CUSTOM}/plugins/${plug_name}"
@@ -18,7 +18,9 @@ plugins=(fzf rust)
 			git clone "https://github.com/${plug_repo}" "$plug_dir"
 		fi
 		plugins+=($plug_name)
-	done
 
-	fpath+=("${ZSH_CUSTOM}/plugins/zsh-completions/src")
+		if [[ $plug_name == 'zsh-completions' ]]; then
+			fpath+=("${ZSH_CUSTOM}/plugins/${plug_name}/src")
+		fi
+	done
 }
