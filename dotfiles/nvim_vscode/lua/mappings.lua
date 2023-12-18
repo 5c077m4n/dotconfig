4ec38f0af4ca30219344e60b5c3b3da1859a7c90
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-12-18 11:29:49 +0200

    Add vscode specific keybindings

--- dotfiles/nvim_vscode/lua/mappings.lua
+++ dotfiles/nvim_vscode/lua/mappings.lua
@@ -1,17 +1,49 @@
 local vscode = require('vscode-neovim')
 
--- Formatting
-vim.keymap.set('n', '<leader>l', function()
-	vscode.action('editor.action.formatDocument')
-end, { desc = 'Format page' })
-vim.keymap.set('v', '<leader>l', function()
-	vscode.call('editor.action.formatSelection')
-end)
+---@param mode string|table
+local function create_keymap_fn(mode)
+	mode = mode or 'n'
+
+	---@param buttons string
+	---@param command string|function
+	---@param options table|nil
+	return function(buttons, command, options)
+		local opts = vim.tbl_extend('force', { remap = true, silent = true }, options or {})
+		vim.keymap.set(mode, buttons, command, opts)
+	end
+end
+
+local nnoremap = create_keymap_fn('n')
+local vnoremap = create_keymap_fn('v')
+
+-- Splits
+--- Traversal
+nnoremap('<C-h>', '<C-w>h')
+nnoremap('<C-j>', '<C-w>j')
+nnoremap('<C-k>', '<C-w>k')
+nnoremap('<C-l>', '<C-w>l')
+--- Creating/removing
+nnoremap('<leader>wq', '<C-w>q', { desc = 'Close split' })
+nnoremap('<leader>wv', vim.cmd.vsplit, { desc = 'New vertical split' })
+nnoremap('<leader>wh', vim.cmd.split, { desc = 'New horizontal split' })
 
 -- Searching
-vim.keymap.set('n', '<leader>f#', function()
+nnoremap('<leader>f#', function()
 	vscode.action('workbench.action.findInFiles', { args = { query = vim.fn.expand('<cword>') } })
 end)
-vim.keymap.set('n', '<leader>fs', function()
+nnoremap('<leader>fs', function()
 	vscode.action('workbench.action.terminal.searchWorkspace')
+end)
+
+-- Filetree
+nnoremap('<leader>tf', function()
+	vscode.action('explorer.openAndPassFocus')
+end)
+
+-- Formatting
+vnoremap('<leader>l', function()
+	vscode.action('editor.action.formatDocument')
+end, { desc = 'Format page' })
+vnoremap('<leader>l', function()
+	vscode.action('editor.action.formatSelection')
 end)
