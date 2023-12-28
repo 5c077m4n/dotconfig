91551ca6b112fda99eeb870d751933b700b94ec0
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-12-28 22:57:02 +0200

    Install fisher

--- dotfiles/fish/conf.d/004_fisher.fish
+++ dotfiles/fish/conf.d/004_fisher.fish
@@ -1,6 +1,4 @@
 set --export --global fisher_path "$XDG_DATA_HOME/fisher"
-<<<<<<< HEAD
-=======
 
 set --global fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
 set --global fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
@@ -8,4 +6,3 @@ set --global fish_complete_path $fish_complete_path[1] $fisher_path/completions 
 for file in $fisher_path/conf.d/*.fish
     source $file
 end
->>>>>>> 5d60e04 (fixup! Add fish config)
