ba903b75877c993117b0b49833d823fc8aee3afe
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2024-02-01 22:25:30 +0200

    Upsert fisher

--- dotfiles/fish/conf.d/004_fisher.fish
+++ dotfiles/fish/conf.d/004_fisher.fish
@@ -1,5 +1,9 @@
 set --export --global fisher_path "$XDG_DATA_HOME/fisher"
 
+if ! type --query fisher
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
+end
+
 set --global fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
 set --global fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
 
