fee6213f697875085ad65bd48edb2d55c0a7070f
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2024-02-24 23:36:45 +0200

    Update lsp config function

--- dotfiles/nvim/lua/vimrc/plugins/lspconfig.lua
+++ dotfiles/nvim/lua/vimrc/plugins/lspconfig.lua
@@ -1,24 +1,3 @@
-local SERVER_LIST = {
-	"denols",
-	"tsserver",
-	"bashls",
-	"lua_ls",
-	"jsonls",
-	"yamlls",
-	"cssls",
-	"html",
-	"tailwindcss",
-	"marksman",
-	"sqlls",
-	"taplo",
-	"dockerls",
-	"gopls",
-	"rust_analyzer",
-	"zls",
-	"jdtls",
-	"pyright",
-}
-
 ---@param options? table
 local function make_config(options)
 	local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -36,7 +15,7 @@ local function make_config(options)
 
 	local base_config = {
 		capabilities = capabilities,
-		flags = { debounce_text_changes = 100 },
+		flags = { debounce_text_changes = 400 },
 	}
 	if type(options) == "table" then
 		base_config = vim.tbl_extend("force", base_config, options)
@@ -44,48 +23,97 @@ local function make_config(options)
 	return base_config
 end
 
-return {
-	SERVER_LIST = SERVER_LIST,
-	setup = function()
-		require("neoconf").setup({})
+---@type table<string, fun(): table | nil>
+local SERVER_CONFIG_MAP = {
+	tsserver = function() end,
+	rust_analyzer = function() end,
+	gopls = function() end,
+	jdtls = function() end,
+	denols = function()
 		local lspconfig = require("lspconfig")
 
-		local servers_needing_lsp_setup = vim.tbl_filter(function(server)
-			return server ~= "tsserver"
-				and server ~= "rust_analyzer"
-				and server ~= "gopls"
-				and server ~= "jdtls"
-		end, SERVER_LIST)
+		vim.g.markdown_fenced_languages = { "ts=typescript" }
+		return make_config({
+			root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc", "deno.lock"),
+		})
+	end,
+	bashls = function()
+		return make_config()
+	end,
+	lua_ls = function()
+		require("neodev").setup({})
 
-		for _, server in pairs(servers_needing_lsp_setup) do
-			local opts = make_config()
-
-			if server == "lua_ls" then
-				require("neodev").setup({})
-
-				opts.settings = {
-					Lua = {
-						completion = { callSnippet = "Replace" },
-						telemetry = { enable = false },
-						workspace = {
-							checkThirdParty = false,
-							library = { "${3rd}/luv/library" },
-						},
-						diagnostics = {
-							globals = { "vim", "string" },
-						},
+		return make_config({
+			settings = {
+				Lua = {
+					completion = { callSnippet = "Replace" },
+					telemetry = { enable = false },
+					workspace = {
+						checkThirdParty = false,
+						library = { "${3rd}/luv/library" },
 					},
-				}
-			elseif server == "tailwindcss" then
-				opts.filetypes = { "javascriptreact", "typescriptreact", "html" }
-				opts.root_dir = lspconfig.util.root_pattern("tailwind.config.js")
-			elseif server == "denols" then
-				vim.g.markdown_fenced_languages = { "ts=typescript" }
-				opts.root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc", "deno.lock")
-			end
+					diagnostics = {
+						globals = { "vim", "string" },
+					},
+				},
+			},
+		})
+	end,
+	jsonls = function()
+		return make_config()
+	end,
+	yamlls = function()
+		return make_config()
+	end,
+	cssls = function()
+		return make_config()
+	end,
+	html = function()
+		return make_config()
+	end,
+	tailwindcss = function()
+		local lspconfig = require("lspconfig")
 
-			lspconfig[server].setup(opts)
-			vim.cmd([[do User LspAttachBuffers]])
+		return make_config({
+			filetypes = { "javascriptreact", "typescriptreact", "html" },
+			root_dir = lspconfig.util.root_pattern("tailwind.config.js"),
+		})
+	end,
+	marksman = function()
+		return make_config()
+	end,
+	sqlls = function()
+		return make_config()
+	end,
+	taplo = function()
+		return make_config()
+	end,
+	dockerls = function()
+		return make_config()
+	end,
+	zls = function()
+		return make_config()
+	end,
+	pyright = function()
+		return make_config()
+	end,
+}
+
+return {
+	SERVER_LIST = vim.tbl_keys(SERVER_CONFIG_MAP),
+	setup = function()
+		local neoconf = require("neoconf")
+		local lspconfig = require("lspconfig")
+
+		neoconf.setup({})
+
+		for server, config_fn in pairs(SERVER_CONFIG_MAP) do
+			local server_config = config_fn()
+
+			if type(server_config) == "table" then
+				lspconfig[server].setup(server_config)
+				vim.cmd([[do User LspAttachBuffers]])
+			end
 		end
 	end,
 }
