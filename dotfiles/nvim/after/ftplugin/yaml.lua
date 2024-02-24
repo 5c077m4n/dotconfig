25ef4b7e5c0688683bfbf26584b2f7439a01939e
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2024-02-25 00:20:35 +0200

    Add yaml filetype config

---
+++ dotfiles/nvim/after/ftplugin/yaml.lua
@@ -0,0 +1,10 @@
+local o = vim.opt_local
+
+o.expandtab = false
+o.shiftwidth = 2
+o.tabstop = 2
+o.softtabstop = 2
+
+-- folds based on indentation https://neovim.io/doc/user/fold.html#fold-indent
+-- if you are a heavy user of folds, consider using `nvim-ufo`
+o.foldmethod = "indent"
