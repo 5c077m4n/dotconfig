7256b6c423dc0c02c4e103e78ca8a1ddece8b419
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2024-03-02 20:41:25 +0200

    Activate poetry venv if needed

--- dotfiles/fish/functions/v..fish
+++ dotfiles/fish/functions/v..fish
@@ -1,3 +1,8 @@
 function v.
+    if type --query poetry && test -f poetry.lock
+        . (poetry env info --path)/bin/activate.fish
+    end
+
     nvim . $argv
+    type --query deactivate && deactivate
 end
