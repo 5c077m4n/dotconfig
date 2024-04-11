e1a266560b21320d32bf101a5ca8e47b16974776
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2024-04-12 01:10:06 +0300

    Add neo tree commands

--- dotfiles/nvim/lua/vimrc/plugins/neotree/commands.lua
+++ dotfiles/nvim/lua/vimrc/plugins/neotree/commands.lua
@@ -1,6 +1,11 @@
 local keymap = require("vimrc.utils").keymapping
 
-keymap.nnoremap("<leader>tf", vim.cmd.Neotree)
+keymap.nnoremap("<leader>tf", vim.cmd.Neotree, { desc = "Focus on curren file in `neo-tree`" })
 keymap.nnoremap("<leader>tt", function()
 	vim.cmd.Neotree("toggle")
-end)
+end, { desc = "Toggle show of `neo-tree`" })
+keymap.nnoremap("<leader>tr", function()
+	require("neo-tree.sources.filesystem.commands").refresh(
+		require("neo-tree.sources.manager").get_state("filesystem")
+	)
+end, { desc = "Refresh `neo-tree`" })
