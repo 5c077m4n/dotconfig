a0263d81c7dbc2645947f7a629386230fdaccb5b
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2024-08-07 08:58:41 +0300

    Add brew for x86_64 arch

--- dotfiles/fish/conf.d/003_homebrew.fish
+++ dotfiles/fish/conf.d/003_homebrew.fish
@@ -1,9 +1,11 @@
-set --local brew_bin_path /opt/homebrew/bin/brew
+set --export --global HOMEBREW_NO_ANALYTICS 1
+set --export --global HOMEBREW_BUNDLE_FILE "$HOME/.config/homebrew/Brewfile"
 
-if test -x $brew_bin_path
-    set --export --global HOMEBREW_NO_ANALYTICS 1
-    set --export --global HOMEBREW_PREFIX ($brew_bin_path --prefix)
-    set --export --global HOMEBREW_BUNDLE_FILE "$HOME/.config/homebrew/Brewfile"
+set --local brew_bin_arm64 /opt/homebrew/bin/brew
+set --local brew_bin_x86 /usr/local/Homebrew/bin/brew
 
-    eval ($brew_bin_path shellenv)
+if test -x $brew_bin_x86 -a (uname -m) != arm64
+    eval ($brew_bin_x86 shellenv)
+else if test -x $brew_bin_arm64
+    eval ($brew_bin_arm64 shellenv)
 end
