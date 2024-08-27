7c4f608417704340581ed3a72d353ad7d2ad9c18
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2024-08-28 00:22:01 +0300

    Update venv init in nvim open func

--- dotfiles/fish/functions/v..fish
+++ dotfiles/fish/functions/v..fish
@@ -1,8 +1,9 @@
-function v. --description "Open Neovim in the current directory and if inside a poetry project activate its env"
+function v. --description "Open Neovim in the current directory and if inside a poetry project activate its virtual environment"
     if type --query poetry && test -f poetry.lock
         . (poetry env info --path)/bin/activate.fish
+        nvim . $argv
+        deactivate
+    else
+        nvim . $argv
     end
-
-    nvim . $argv
-    type --query deactivate && deactivate
 end
