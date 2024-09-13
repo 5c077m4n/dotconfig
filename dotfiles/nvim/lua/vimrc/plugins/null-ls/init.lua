6891cdd925c53bc30e6da0ffa98cbe3f7123ea9f
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2024-09-13 18:03:03 +0300

    Remove golnagci-lint from null-ls

--- dotfiles/nvim/lua/vimrc/plugins/null-ls/init.lua
+++ dotfiles/nvim/lua/vimrc/plugins/null-ls/init.lua
@@ -34,7 +34,6 @@ local sources = {
 	null_ls.builtins.formatting.sqlfluff,
 	null_ls.builtins.diagnostics.sqlfluff,
 	-- Go
-	null_ls.builtins.diagnostics.golangci_lint,
 	null_ls.builtins.formatting.gofumpt,
 	null_ls.builtins.formatting.goimports,
 	null_ls.builtins.formatting.goimports_reviser,
