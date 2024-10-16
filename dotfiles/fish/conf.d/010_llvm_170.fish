44c5160fc8841f6f9f10e0ccd2dafcda90faaf98
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2024-10-16 21:56:36 +0300

    Remove llvm fish config

--- dotfiles/fish/conf.d/010_llvm_170.fish
+++
@@ -1,8 +0,0 @@
-set --local llvm_config (brew --prefix llvm@17)/bin/llvm-config
-
-if test -x $llvm_config
-    set --local llvm_config_prefix ($llvm_config --prefix)
-
-    set --global --export LLVM_SYS_170_PREFIX $llvm_config_prefix
-    set --global --export LDFLAGS -L$HOMEBREW_PREFIX/opt/llvm/lib/c++ -Wl,-rpath,$HOMEBREW_PREFIX/opt/llvm/lib/c++
-end
