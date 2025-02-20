ade8084e0d4958eb054711835251460d5b20e5e8
Roee <35409124+5c077m4n@users.noreply.github.com>
2025-02-20 10:11:05 +0200

    Show polybar on all screens

--- dotfiles/.config/polybar/launch.sh
+++ dotfiles/.config/polybar/launch.sh
@@ -4,11 +4,12 @@ set -euo pipefail
 
 # Terminate already running bar instances
 killall -q polybar || true
-killall -q i3blocks || true
-killall -q i3status || true
 
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location `~/.config/polybar/config.ini`
-polybar --reload &
+# Launch Polybar, using default config location `~/.config/polybar/config.ini` on all monitors
+for m in $(polybar --list-monitors | cut -d':' -f1); do
+	MONITOR="$m" polybar --reload &
+done
+unset m
