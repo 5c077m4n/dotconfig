cb948529fa8cc344b01cc897eeab8a5f3df8f663
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:11 +0300

    Use vim.cmd api for commands

--- dotfiles/nvim/lua/vimrc/plugins/ranger.lua
+++ dotfiles/nvim/lua/vimrc/plugins/ranger.lua
@@ -1,7 +1,7 @@
 local keymap = require('vimrc.utils.keymapping')
 local g = vim.g
 
-keymap.nnoremap('<leader>rr', ':Ranger<CR>')
+keymap.nnoremap('<leader>rr', vim.cmd.Ranger)
 
 g.ranger_map_keys = 0
 g.ranger_command_override = [[ranger --cmd "set show_hidden=true"]]
