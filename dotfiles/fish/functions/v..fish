e5c2b20860bfa252ae41b3a24107b7daaf065756
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:44 +0300

    Add `v.` description

--- dotfiles/fish/functions/v..fish
+++ dotfiles/fish/functions/v..fish
@@ -1,4 +1,4 @@
-function v.
+function v. --description "Open Neovim in the current directory and if inside a poetry project activate its env"
     if type --query poetry && test -f poetry.lock
         . (poetry env info --path)/bin/activate.fish
     end
