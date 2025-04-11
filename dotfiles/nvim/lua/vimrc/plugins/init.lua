f6da25fd4a8043d9e6549a342be659b0ae5d6050
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:43 +0300

    Add session persist

--- dotfiles/nvim/lua/vimrc/plugins/init.lua
+++ dotfiles/nvim/lua/vimrc/plugins/init.lua
@@ -314,6 +314,34 @@ local function setup()
 			end,
 		},
 		{
+			"folke/persistence.nvim",
+			event = { "VeryLazy" },
+			config = function()
+				local persist = require("persistence")
+
+				persist.setup({
+					pre_save = function()
+						vim.cmd.Neotree("close")
+					end,
+					pre_load = function()
+						vim.cmd.Neotree("close")
+					end,
+				})
+				keymap.nnoremap(
+					"<leader>pr",
+					persist.load,
+					{ desc = "Restore the session for the current directory" }
+				)
+				keymap.nnoremap("<leader>pl", function()
+					persist.load({ last = true })
+				end, { desc = "Restore the last session" })
+				keymap.nnoremap("<leader>pd", function()
+					persist.stop()
+					vim.notify("Auto session save disabled")
+				end, { desc = "Disable session save on exit" })
+			end,
+		},
+		{
 			"folke/neodev.nvim",
 			dependencies = { "folke/neoconf.nvim" },
 			event = { "VeryLazy" },
