6cac42de61697c19b9c1a7bc15e1f95ddfea313b
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:41 +0300

    Add env var to disable nextjs telemtry

--- dotfiles/fish/conf.d/001_env.fish
+++ dotfiles/fish/conf.d/001_env.fish
@@ -3,3 +3,5 @@ set --export --global LANG "en_US.UTF-8"
 set --export --global EDITOR nvim
 # Apple
 set --export --global SHELL_SESSIONS_DISABLE 1 # Disables Apple's shell session storage
+# NextJS
+set --export --global NEXT_TELEMETRY_DISABLED 1
