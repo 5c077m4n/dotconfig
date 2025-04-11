29e688dd09a857a66f68702bea532c5a68dcd4e9
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:34 +0300

    Allow alt+f command

--- dotfiles/zsh/config/comple.zsh
+++ dotfiles/zsh/config/comple.zsh
@@ -1,7 +1,6 @@
-# Completions
-## Kubectl
+# Kubectl
 [[ -x "$(command -v kubectl)" ]] && source <(kubectl completion zsh)
-## Kitty search
+# Kitty search
 [[ -x "$(command -v kitty)" ]] && compdef _rg hg
 
 # `zsh-history-substring-search` keymaps
@@ -9,3 +8,18 @@ bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
 bindkey -M vicmd 'k' history-substring-search-up
 bindkey -M vicmd 'j' history-substring-search-down
+
+# Reduce `vi-cmd-mode` sensitivity
+vi-cmd-mode() {
+	local is_esc=1 REPLY
+	while (( KEYS_QUEUED_COUNT || PENDING )); do
+		is_esc=0
+		zle read-command
+	done
+	((is_esc)) && zle .$WIDGET
+}
+zle -N vi-cmd-mode
+KEYTIMEOUT=10
+
+bindkey '^[f' forward-word
+bindkey '^[b' backward-word
