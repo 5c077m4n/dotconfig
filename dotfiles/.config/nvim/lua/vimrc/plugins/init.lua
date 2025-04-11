f09da294c29afccd8dc1d2de45040a51fb2c898e
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:55 +0300

    Add ai code companion nvim plugin

--- dotfiles/.config/nvim/lua/vimrc/plugins/init.lua
+++ dotfiles/.config/nvim/lua/vimrc/plugins/init.lua
@@ -342,6 +342,9 @@ local function setup()
 							score_offset = 100,
 						},
 					},
+					per_filetype = {
+						codecompanion = { "codecompanion" },
+					},
 				},
 				signature = {
 					enabled = false,
@@ -450,6 +453,50 @@ local function setup()
 			config = function()
 				require("vimrc.plugins.telescope")
 			end,
+		},
+		-- AI
+		{
+			"olimorris/codecompanion.nvim",
+			event = { "VeryLazy" },
+			config = function()
+				require("codecompanion").setup({
+					adapters = {
+						deepseek_r1_small = function()
+							return require("codecompanion.adapters").extend("ollama", {
+								name = "deepseek-r1:1.5b",
+								schema = {
+									model = { default = "deepseek-r1:1.5b" },
+									num_ctx = { default = 16384 },
+									num_predict = { default = -1 },
+								},
+							})
+						end,
+						qwen_coder_small = function()
+							return require("codecompanion.adapters").extend("ollama", {
+								name = "qwen2.5-coder:1.5b",
+								schema = {
+									model = { default = "qwen2.5-coder:1.5b" },
+									num_ctx = { default = 16384 },
+									num_predict = { default = -1 },
+								},
+							})
+						end,
+					},
+					strategies = {
+						chat = { adapter = "anthropic" },
+						inline = { adapter = "anthropic" },
+						cmd = { adapter = "anthropic" },
+					},
+					display = {
+						chat = { show_settings = true },
+						inline = {
+							---@type "vertical" | "horizontal" | "buffer"
+							layout = "buffer",
+						},
+					},
+				})
+			end,
+			dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
 		},
 	}, { ui = { border = "single" } })
 end
