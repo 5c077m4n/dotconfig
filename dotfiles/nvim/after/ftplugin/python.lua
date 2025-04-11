6e38c969b832aa77efe061ac416866a27dd7c876
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:41 +0300

    Add python filetype local config

---
+++ dotfiles/nvim/after/ftplugin/python.lua
@@ -0,0 +1,23 @@
+local o = vim.opt_local
+local cmd = vim.cmd
+
+-- use pep8 standards
+o.expandtab = true
+o.shiftwidth = 4
+o.tabstop = 4
+o.softtabstop = 4
+
+-- folds based on indentation https://neovim.io/doc/user/fold.html#fold-indent
+-- if you are a heavy user of folds, consider using `nvim-ufo`
+o.foldmethod = "indent"
+
+-- automatically capitalize boolean values. Useful if you come from a
+-- different language, and lowercase them out of habit.
+cmd.inoreabbrev("<buffer> true True")
+cmd.inoreabbrev("<buffer> false False")
+
+-- in the same way, we can fix habits regarding comments or None
+cmd.inoreabbrev("<buffer> -- #")
+cmd.inoreabbrev("<buffer> null None")
+cmd.inoreabbrev("<buffer> none None")
+cmd.inoreabbrev("<buffer> nil None")
