fa670f15d18970e49418827dfe40d1024e04fbc8
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:40 +0300

    Add cursor shapes for different vi modes

---
+++ dotfiles/fish/conf.d/009_cursor.fish
@@ -0,0 +1,5 @@
+# Set the cursor shapes for the different vi modes.
+set --global fish_cursor_default block blink
+set --global fish_cursor_insert line blink
+set --global fish_cursor_replace_one underscore blink
+set --global fish_cursor_visual block
