84e30ce02b51e792071d483b084937e1b9a3a92b
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:51 +0300

    Enable tlp service

--- flake/configuration.nix
+++ flake/configuration.nix
@@ -107,6 +107,29 @@ in
       #media-session.enable = true;
     };
 
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 80;
+
+        # [Optional] helps save long term battery health
+        START_CHARGE_THRESH_BAT0 = 50; # bellow it starts to charge
+        STOP_CHARGE_THRESH_BAT0 = 80; # above it stops charging
+
+        # Always run on battery mode (in case of overheating on AC power)
+        #TLP_DEFAULT_MODE = "BAT";
+        #TLP_PERSISTENT_DEFAULT = 1;
+      };
+    };
   };
 
   hardware = {
