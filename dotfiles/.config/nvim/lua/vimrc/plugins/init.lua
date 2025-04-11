2df01f07c86bb018f96f5c6123819a79266fad36
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:52 +0300

    Update blink.cmp config struct

--- dotfiles/.config/nvim/lua/vimrc/plugins/init.lua
+++ dotfiles/.config/nvim/lua/vimrc/plugins/init.lua
@@ -310,14 +310,6 @@ local function setup()
 			---@module 'blink.cmp'
 			---@type blink.cmp.Config
 			opts = {
-				keymap = {
-					preset = "default",
-					["<CR>"] = { "accept", "fallback" },
-
-					cmdline = {
-						preset = "default",
-					},
-				},
 				appearance = {
 					use_nvim_cmp_as_default = true,
 					nerd_font_variant = "mono",
