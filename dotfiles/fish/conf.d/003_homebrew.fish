6b5383518b94ca2f20ca0bbc76222ff94887f91d
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:41 +0300

    Move homebrew config into its own dir

--- dotfiles/fish/conf.d/003_homebrew.fish
+++ dotfiles/fish/conf.d/003_homebrew.fish
@@ -3,7 +3,7 @@ set --local brew_bin_path /opt/homebrew/bin/brew
 if test -x $brew_bin_path
     set --export --global HOMEBREW_NO_ANALYTICS 1
     set --export --global HOMEBREW_PREFIX ($brew_bin_path --prefix)
-    set --export --global HOMEBREW_BUNDLE_FILE "$HOME/repos/dotconfig/assets/macos/Brewfile"
+    set --export --global HOMEBREW_BUNDLE_FILE "$HOME/.config/homebrew/Brewfile"
 
     eval ($brew_bin_path shellenv)
 end
