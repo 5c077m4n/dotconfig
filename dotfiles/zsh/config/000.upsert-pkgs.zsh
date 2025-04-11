51d061f7c65b0417d18958ac39fda65ecf27bf49
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:35 +0300

    Add xdg dirs upsert

--- dotfiles/zsh/config/000.upsert-pkgs.zsh
+++ dotfiles/zsh/config/000.upsert-pkgs.zsh
@@ -9,3 +9,10 @@
 if [[ ! -f "${ZSH}/oh-my-zsh.sh" ]]; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
+
+() {
+	local xdg_dir
+	for xdg_dir in $(printenv | grep '^XDG_.*_HOME=' | sed 's/.*=//'); do
+		[[ ! -d "$xdg_dir" ]] && mkdir -p "$xdg_dir"
+	done
+}
