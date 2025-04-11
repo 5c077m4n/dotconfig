84b43c4cb19af48d3db51cc83fd892c14b5da78e
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:05 +0300

    Re-write gen userjs in python

--- assets/firefox/gen-user-js.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-() {
-	local arkenfox_user="$(curl https://raw.githubusercontent.com/arkenfox/user.js/master/user.js)"
-	local __dirname="$1"
-	local ff_profile_dir="${2:-"."}"
-
-	{ echo $arkenfox_user; echo "\n"; cat "${__dirname}/user-overrides.js"; } > "${ff_profile_dir}/user.js"
-} "${0:A:h}" "$@"
