4f3a4d22d6be28faea7b86918d9e3711f552abf9
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:49 +0300

    Check no nix before installing TPM

--- dotfiles/fish/conf.d/004_tmux.fish
+++ dotfiles/fish/conf.d/004_tmux.fish
@@ -1,7 +1,7 @@
 set --local tmux_plugin_path $XDG_STATE_HOME/tmux/plugins/
 set --local tpm_path $tmux_plugin_path/tpm/
 
-if type --query tmux && ! test -d $tpm_path
+if ! type --query nix && type --query tmux && ! test -d $tpm_path
     mkdir -p $tmux_plugin_path
     git -C $tmux_plugin_path clone https://github.com/tmux-plugins/tpm
 end
