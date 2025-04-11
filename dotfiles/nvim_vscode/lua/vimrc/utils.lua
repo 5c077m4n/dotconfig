475296636d276ee7a9aebae537c926ac539a543b
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:39 +0300

    Cleanup

--- dotfiles/nvim_vscode/lua/vimrc/utils.lua
+++ dotfiles/nvim_vscode/lua/vimrc/utils.lua
@@ -6,16 +6,12 @@ local function create_keymap_fn(mode)
 	---@param command string|function
 	---@param options? table
 	return function(buttons, command, options)
-		local opts = vim.tbl_extend("force", { noremap = true, silent = true }, options or {})
+		local opts = vim.tbl_extend("force", { remap = true, silent = true }, options or {})
 		vim.keymap.set(mode, buttons, command, opts)
 	end
 end
 
 return {
 	nnoremap = create_keymap_fn("n"),
-	nvnoremap = create_keymap_fn({ "n", "v" }),
 	vnoremap = create_keymap_fn("v"),
-	inoremap = create_keymap_fn("i"),
-	tnoremap = create_keymap_fn("t"),
-	snoremap = create_keymap_fn("s"),
 }
