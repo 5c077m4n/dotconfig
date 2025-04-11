afd1a0c46d12918982586c1e5ccd33f90e7f8be3
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:43 +0300

    Remove hop inline jump

--- dotfiles/nvim/lua/vimrc/plugins/init.lua
+++ dotfiles/nvim/lua/vimrc/plugins/init.lua
@@ -388,36 +388,9 @@ local function setup()
 			lazy = true,
 			config = function()
 				local hop = require("hop")
-				local directions = require("hop.hint").HintDirection
 
 				hop.setup({ keys = "etovxqpdygfblzhckisuran" })
 				keymap.nvnoremap("<C-f>", hop.hint_words)
-				keymap.nvnoremap("f", function()
-					hop.hint_char1({
-						direction = directions.AFTER_CURSOR,
-						current_line_only = true,
-					})
-				end)
-				keymap.nvnoremap("F", function()
-					hop.hint_char1({
-						direction = directions.BEFORE_CURSOR,
-						current_line_only = true,
-					})
-				end)
-				keymap.nvnoremap("t", function()
-					hop.hint_char1({
-						direction = directions.AFTER_CURSOR,
-						current_line_only = true,
-						hint_offset = -1,
-					})
-				end)
-				keymap.nvnoremap("T", function()
-					hop.hint_char1({
-						direction = directions.BEFORE_CURSOR,
-						current_line_only = true,
-						hint_offset = 1,
-					})
-				end)
 			end,
 		},
 		{
