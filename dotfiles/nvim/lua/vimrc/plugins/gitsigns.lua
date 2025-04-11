9d20d90be14acb3371a37da89bbf5eba096b31c3
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:44 +0300

    Remove unneeded `signsigns` config

--- dotfiles/nvim/lua/vimrc/plugins/gitsigns.lua
+++ dotfiles/nvim/lua/vimrc/plugins/gitsigns.lua
@@ -4,38 +4,6 @@ local gitsigns_actions = require("gitsigns.actions")
 local keymap = require("vimrc.utils").keymapping
 
 gitsigns.setup({
-	signs = {
-		add = {
-			hl = "GitSignsAdd",
-			text = "+",
-			numhl = "GitSignsAddNr",
-			linehl = "GitSignsAddLn",
-		},
-		change = {
-			hl = "GitSignsChange",
-			text = "│",
-			numhl = "GitSignsChangeNr",
-			linehl = "GitSignsChangeLn",
-		},
-		delete = {
-			hl = "GitSignsDelete",
-			text = "_",
-			numhl = "GitSignsDeleteNr",
-			linehl = "GitSignsDeleteLn",
-		},
-		topdelete = {
-			hl = "GitSignsDelete",
-			text = "‾",
-			numhl = "GitSignsDeleteNr",
-			linehl = "GitSignsDeleteLn",
-		},
-		changedelete = {
-			hl = "GitSignsChange",
-			text = "~",
-			numhl = "GitSignsChangeNr",
-			linehl = "GitSignsChangeLn",
-		},
-	},
 	watch_gitdir = { interval = 800, follow_files = true },
 	update_debounce = 400,
 	on_attach = function(buffer_num)
