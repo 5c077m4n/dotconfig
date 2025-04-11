514647c720e4c694d2c143dc926f820d19dca1b5
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:06 +0300

    Lint py

--- assets/firefox/gen-user-js.py
+++ assets/firefox/gen-user-js.py
@@ -3,33 +3,42 @@ import sys
 from pathlib import Path
 from urllib.request import urlopen
 
-key_value_pairs = re.compile(
-    r'^\s*user_pref\((?P<key>[a-zA-Z0-9"\'._]+),\s*(?P<value>[a-zA-Z0-9"\'._]+)\)',
-    re.MULTILINE,
-)
-response = urlopen("https://raw.githubusercontent.com/arkenfox/user.js/master/user.js")
-response_raw = response.read()
-userjs_origin = response_raw.decode("utf-8")
 
-dest = Path(sys.argv[1] if len(sys.argv) > 1 else ".").joinpath("user.js").resolve()
+def main():
+    key_value_pairs = re.compile(
+        r'^\s*user_pref\((?P<key>[a-zA-Z0-9"\'._]+),\s*(?P<value>[a-zA-Z0-9"\'._]+)\)',
+        re.MULTILINE,
+    )
+    response = urlopen(
+        "https://raw.githubusercontent.com/arkenfox/user.js/master/user.js"
+    )
+    response_raw = response.read()
+    userjs_origin = response_raw.decode("utf-8")
 
-with open(dest, "w") as userjs_final:
-    print(f"Starting user.js write in '{dest}'")
+    dest = Path(sys.argv[1] if len(sys.argv) > 1 else ".").joinpath("user.js").resolve()
 
-    matches_iter = key_value_pairs.finditer(userjs_origin)
-    for m in matches_iter:
-        userjs_final.write(f"user_pref({m.group('key')}, {m.group('value')});\n")
+    with open(dest, "w") as userjs_final:
+        print(f"Starting user.js write in '{dest}'")
 
-    __dirname = Path(__file__).parent.resolve()
-    overrides_path = __dirname.joinpath("user-overrides.js")
+        matches_iter = key_value_pairs.finditer(userjs_origin)
+        for m in matches_iter:
+            userjs_final.write(f"user_pref({m.group('key')}, {m.group('value')});\n")
 
-    with open(overrides_path, "r") as overrides:
-        content = overrides.read()
-        overrides_iter = key_value_pairs.finditer(content)
+        __dirname = Path(__file__).parent.resolve()
+        overrides_path = __dirname.joinpath("user-overrides.js")
 
-        userjs_final.write("\n\n/** OVERRIDES */\n")
-        for o in overrides_iter:
-            userjs_final.write(f"user_pref({o.group('key')}, {o.group('value')});\n")
+        with open(overrides_path, "r") as overrides:
+            content = overrides.read()
+            overrides_iter = key_value_pairs.finditer(content)
 
-    print("user.js file write complete 🤗")
+            userjs_final.write("\n\n/** OVERRIDES */\n")
+            for o in overrides_iter:
+                userjs_final.write(
+                    f"user_pref({o.group('key')}, {o.group('value')});\n"
+                )
 
+        print("user.js file write complete 🤗")
+
+
+if __name__ == "__main__":
+    main()
