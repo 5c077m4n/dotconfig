16fcd9216fe07f5bc6dd8390e63bfb9db31c14ac
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:38 +0300

    Disable plugins if in vscode

--- dotfiles/nvim/lua/vimrc.lua
+++ dotfiles/nvim/lua/vimrc.lua
@@ -1,8 +1,11 @@
-vim.loader.enable()
-
 require('vimrc.options')
 require('vimrc.filetypes')
 require('vimrc.mappings')
 require('vimrc.augroups')
-require('vimrc.plugins').setup()
-require('vimrc.utils').signals.handle_usr1()
+
+if not vim.g.vscode then
+	vim.loader.enable()
+
+	require('vimrc.plugins').setup()
+	require('vimrc.utils').signals.handle_usr1()
+end
