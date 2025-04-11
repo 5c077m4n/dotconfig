a8154d89e12e9a521b3f5363e4a674f4f3d26298
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:35 +0300

    Move completions to config load end

--- dotfiles/zsh/config/005.path.zsh
+++ dotfiles/zsh/config/005.path.zsh
@@ -1,9 +1,6 @@
 if [[ -d "${KREW_ROOT:-$HOME/.krew}/bin" ]]; then
 	path+=("${KREW_ROOT:-$HOME/.krew}/bin")
 fi
-if [[ -x "$(command -v kubectl)" ]]; then
-	source <(kubectl completion zsh)
-fi
 
 path+=("${HOME}/.local/bin" "${GOBIN}")
 cdpath+=("${HOME}/repos")
