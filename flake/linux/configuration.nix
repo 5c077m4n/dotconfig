c6be7cf53f0bbf2d65dbd7439a07d9f7226bfce8
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:53 +0300

    Replace tlp with auto-cpufreq

--- flake/linux/configuration.nix
+++ flake/linux/configuration.nix
@@ -110,36 +110,14 @@ in
       wireplumber.enable = true;
     };
 
-    tlp = {
-      enable = true;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-        CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 100;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 80;
-
-        # [Optional] helps save long term battery health
-        START_CHARGE_THRESH_BAT0 = 40; # below it start to charge
-        STOP_CHARGE_THRESH_BAT0 = 80; # above it stop charging
-
-        # Always run on battery mode (in case of overheating on AC power)
-        #TLP_DEFAULT_MODE = "BAT";
-        #TLP_PERSISTENT_DEFAULT = 1;
-      };
-    };
+    auto-cpufreq.enable = true;
   };
 
   hardware = {
     graphics = {
       enable = true;
-      extraPackages = with pkgs; [
-        vpl-gpu-rt # intel graphics driver
+      extraPackages = [
+        pkgs.vpl-gpu-rt # intel graphics driver
       ];
     };
     pulseaudio.enable = false;
