5eb6973c9bb0b51ff02b4353a67d0cf9906719a4
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:48 +0300

    Use `nil_ls` only if nix is installed

--- dotfiles/nvim/lua/vimrc/plugins/lspconfig.lua
+++ dotfiles/nvim/lua/vimrc/plugins/lspconfig.lua
@@ -143,13 +143,15 @@ local SERVER_CONFIG_MAP = {
 		return make_config({ filetypes = { "go", "gomod" } })
 	end,
 	nil_ls = function()
-		return make_config({
-			settings = {
-				nix = {
-					flake = { autoArchive = true },
+		if vim.fn.executable("nix") == 1 then
+			return make_config({
+				settings = {
+					nix = {
+						flake = { autoArchive = true },
+					},
 				},
-			},
-		})
+			})
+		end
 	end,
 	jsonls = function()
 		local schemastore = require("schemastore")
