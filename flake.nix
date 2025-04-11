6f97fb45cc6ebd10ac799b48da22e8ecd01aacfa
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:48 +0300

    Bump nixpkgs version

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "MacOS system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin = {
       url = "github:lnl7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
