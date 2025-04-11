563e06f1891d01758ad9d5e81a45f3cfcd2d9756
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:45 +0300

    Move config into own files

--- dotfiles/zsh/conf.d/009.go.zsh
+++ dotfiles/zsh/conf.d/009.go.zsh
@@ -1,4 +1,7 @@
 if (( $+commands[go] )); then
+	export GOPATH="${XDG_DATA_HOME}/go"
+	export GOBIN="${GOPATH}/bin"
 	export GOROOT="$(brew --prefix go)/libexec"
+
 	typeset -TU GOPATH gopath
 fi
---
+++ dotfiles/zsh/conf.d/013.starship.zsh
@@ -0,0 +1,1 @@
+export STARSHIP_CONFIG="${XDG_CONFIG_HOME}/starship/config.toml"
