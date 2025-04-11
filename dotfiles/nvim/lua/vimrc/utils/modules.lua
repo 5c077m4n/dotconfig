008c22044b933f4a9a296a52bedb17ea840bb96f
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:11 +0300

    Remove dead code

--- dotfiles/nvim/lua/vimrc/utils/modules.lua
+++ dotfiles/nvim/lua/vimrc/utils/modules.lua
@@ -14,13 +14,4 @@ function M.update_vimrc()
 	utils.async_cmd({ 'git', '-C', vim.env.HOME .. '/.config/nvim', 'pull', '--force', 'origin', 'master' })
 end
 
-function M.yarn_global_install(pkg_name_list)
-	utils.async_cmd({ 'yarn', 'global', 'add', unpack(pkg_name_list) })
-end
-
-function M.is_plugin_loaded(pkg_name)
-	---@diagnostic disable-next-line: undefined-global
-	return packer_plugins and packer_plugins[pkg_name] and packer_plugins[pkg_name].loaded
-end
-
 return M
