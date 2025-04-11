cabe81465366848924b57865232b6903e6b32dc5
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:53 +0300

    Force polybar trey module to be only on the primary monitor

--- dotfiles/.config/polybar/launch.sh
+++ dotfiles/.config/polybar/launch.sh
@@ -9,7 +9,9 @@ killall -q polybar || true
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location `~/.config/polybar/config.ini` on all monitors
-for m in $(polybar --list-monitors | cut -d':' -f1); do
-	MONITOR="$m" polybar --reload &
+# Launch bar on each monitor, tray on primary
+polybar --list-monitors | while IFS=$'\n' read line; do
+	monitor=$(echo "$line" | cut -d':' -f1)
+	primary=$(echo "$line" | cut -d' ' -f3)
+	MONITOR=$monitor polybar --reload "top${primary:+"-primary"}" &
 done
-unset m
