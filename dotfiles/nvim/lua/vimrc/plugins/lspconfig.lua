cb4c4c3cd750cab4cd5ade1f610eef534b36df6b
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:48 +0300

    Docs comment lint

--- dotfiles/nvim/lua/vimrc/plugins/lspconfig.lua
+++ dotfiles/nvim/lua/vimrc/plugins/lspconfig.lua
@@ -175,7 +175,7 @@ local SERVER_CONFIG_MAP = {
 						-- You must disable built-in schemaStore support if you want to use
 						-- this plugin and its advanced options like `ignore`.
 						enable = false,
-						-- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+						-- Avoid "TypeError: Cannot read properties of undefined (reading 'length')"
 						url = "",
 					},
 					schemas = schemastore.yaml.schemas(),
