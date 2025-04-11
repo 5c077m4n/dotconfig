a771506482ed24be887761a4c4e25f9443782551
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:42 +0300

    Update prettier config

--- dotfiles/fish/conf.d/001_env.fish
+++ dotfiles/fish/conf.d/001_env.fish
@@ -2,6 +2,7 @@
 set --export --global LANG "en_US.UTF-8"
 set --export --global EDITOR nvim
 # Apple
-set --export --global SHELL_SESSIONS_DISABLE 1 # Disables Apple's shell session storage
+## Disables Apple's shell session storage
+set --export --global SHELL_SESSIONS_DISABLE 1
 # NextJS
 set --export --global NEXT_TELEMETRY_DISABLED 1
