37d970f4ffcd322bb602d2e3da970eade10fe71e
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:07 +0300

    Add fzf config

--- dotfiles/zsh/fzf-functions.zsh
+++ dotfiles/zsh/fzf-functions.zsh
@@ -6,6 +6,15 @@ is_in_git_repo() {
 }
 
 export FZF_DEFAULT_OPTS="--bind ctrl-u:preview-page-up --bind ctrl-d:preview-page-down --bind ctrl-/:toggle-preview"
+# Use fd (https://github.com/sharkdp/fd) instead of the default find command for listing path candidates.
+_fzf_compgen_path() {
+  fd --hidden --follow --exclude ".git" . "$1"
+}
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude ".git" . "$1"
+}
+
 __fzf_down() {
 	zle reset-prompt
 	_fzf_complete \
@@ -64,9 +73,25 @@ _fzf_complete_nr() {
 	is_in_git_repo && [[ -f package.json ]] || return
 
 	__fzf_down \
-		--preview "cat package.json | jq -r '.scripts.\"{}\"' | bat --force-colorization --style='plain' --language='bash'" \
+		--preview "jq -r '.scripts.\"{}\"' package.json | bat --force-colorization --style='plain' --language='bash'" \
 		--preview-window 'top:25%' \
 		-- "$@" < <(
-			cat package.json | jq -r ".scripts | keys | .[]"
+			jq -r ".scripts | keys | .[]" package.json
 		)
+}
+# `npm run **<Tab>`
+_fzf_complete_npm () {
+	is_in_git_repo && [[ -f package.json ]] || return
+
+	local args="$*"
+	if [[ $args == 'npm run '* ]]; then
+		__fzf_down \
+			--preview "jq -r '.scripts.\"{}\"' package.json | bat --force-colorization --style='plain' --language='bash'" \
+			--preview-window 'top:25%' \
+			-- "$@" < <(
+				jq --raw-output ".scripts | keys | .[]" package.json
+			)
+	else
+		eval "zle ${fzf_default_completion:-expand-or-complete}"
+	fi
 }
