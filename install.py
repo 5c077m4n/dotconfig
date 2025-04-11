5cc06d7c87ed6fa6fde285ca2175ec2a7d764bb0
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-04-11 23:51:39 +0300

    Add home dir config files

--- install.py
+++ install.py
@@ -18,32 +18,34 @@ def main() -> None:
     home_dir = Path().home()
     __dirname = Path(__file__).parent.absolute()
 
-    with home_dir.joinpath(".zshenv").open("w") as zshenv_write:
-        zshenv_write.writelines(
-            [
-                'export ZDOTDIR="${HOME}/.config/zsh"\n',
-                '[[ -f "${ZDOTDIR}/.zshenv" ]] && . "${ZDOTDIR}/.zshenv"\n',
-            ]
-        )
+    # Link legacy config files into `$HOME`
+    home_dotfiles_dir = __dirname.joinpath("home_dotfiles")
+    for source_dir in home_dotfiles_dir.iterdir():
+        dest_dir = home_dir.joinpath(source_dir.parts[-1])
+
+        wipe(dir=dest_dir)
+        dest_dir.symlink_to(target=source_dir, target_is_directory=False)
 
     config_dir = home_dir.joinpath(".config")
     if not config_dir.is_dir():
         mkdir(config_dir)
 
+    # Link config files into `$XDG_CONFIG_HOME`
     dotfiles_dir = __dirname.joinpath("dotfiles")
     for source_dir in dotfiles_dir.iterdir():
         dest_dir = config_dir.joinpath(source_dir.parts[-1])
 
-        wipe(dest_dir)
-        dest_dir.symlink_to(source_dir, True)
+        wipe(dir=dest_dir)
+        dest_dir.symlink_to(target=source_dir, target_is_directory=True)
 
+    # Link scripts to appear in `$PATH`
     bin_dir = __dirname.joinpath("bin")
     local_dir = home_dir.joinpath(".local")
     if not local_dir.exists():
         mkdir(local_dir)
     dest_bin_dir = local_dir.joinpath("bin")
-    wipe(dest_bin_dir)
-    dest_bin_dir.symlink_to(bin_dir)
+    wipe(dir=dest_bin_dir)
+    dest_bin_dir.symlink_to(target=bin_dir, target_is_directory=True)
 
 
 if __name__ == "__main__":
