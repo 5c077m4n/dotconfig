d94e864b594b7029f4ef5694aa874f1e2603001b
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-06-06 23:01:11 +0300

    Remove .git from lua root marker

--- dotfiles/.config/nvim/lsp/lua_ls.lua
+++ dotfiles/.config/nvim/lsp/lua_ls.lua
@@ -21,11 +21,11 @@ return {
 				library = {
 					vim.env.VIMRUNTIME,
 					-- Depending on the usage, you might want to add additional paths here.
-					-- "${3rd}/luv/library"
-					-- "${3rd}/busted/library",
+					--"${3rd}/luv/library"
+					--"${3rd}/busted/library",
 				},
 				-- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
-				-- library = vim.api.nvim_get_runtime_file("", true)
+				--library = vim.api.nvim_get_runtime_file("", true)
 			},
 		})
 	end,
@@ -37,7 +37,6 @@ return {
 		"stylua.toml",
 		"selene.toml",
 		"selene.yml",
-		".git",
 	},
 	settings = {
 		Lua = {
