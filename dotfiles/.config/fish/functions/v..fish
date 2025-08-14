41b776fbef24c2cb89b17cabae3a20b7dbcc1663
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-08-14 21:10:14 +0300

    Lint

--- dotfiles/.config/fish/functions/v..fish
+++ dotfiles/.config/fish/functions/v..fish
@@ -1,10 +1,10 @@
 function v. --wraps nvim --description "Open Neovim in the current directory and if inside a poetry project activate its virtual environment"
     if type --query poetry && test -f poetry.lock
-        . (poetry env info --path)/bin/activate.fish
+        source (poetry env info --path)/bin/activate.fish
         nvim . $argv
         deactivate
     else if test -f ./.venv/bin/activate.fish
-        . ./.venv/bin/activate.fish
+        source ./.venv/bin/activate.fish
         nvim . $argv
         deactivate
     else
