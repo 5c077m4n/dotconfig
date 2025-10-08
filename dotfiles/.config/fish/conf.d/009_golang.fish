f4e480015e98b0ca0d8046d0fc02aeff4265c377
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2025-10-08 18:53:01 +0300

    Fix go env vars

--- dotfiles/.config/fish/conf.d/009_golang.fish
+++ dotfiles/.config/fish/conf.d/009_golang.fish
@@ -1,6 +1,7 @@
 if type --query go
     set --export --global GOPATH "$XDG_DATA_HOME/go"
     set --export --global GOBIN "$GOPATH/bin"
+    set --export --global GOPROXY direct
 
     fish_add_path $GOBIN
 end
--- dotfiles/.config/zsh/conf.d/009.go.zsh
+++ dotfiles/.config/zsh/conf.d/009.go.zsh
@@ -1,7 +1,6 @@
 if (( $+commands[go] )); then
 	export GOPATH="${XDG_DATA_HOME}/go"
 	export GOBIN="${GOPATH}/bin"
-	export GOROOT="$(brew --prefix go)/libexec"
 
 	typeset -TU GOPATH gopath
 fi
