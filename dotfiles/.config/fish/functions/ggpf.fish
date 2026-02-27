f91dbae7a31f3e4e4c352cf2a6f9dfc485a93222
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2026-02-27 08:55:00 +0200

    Add git push force check

--- dotfiles/.config/fish/functions/ggpf.fish
+++ dotfiles/.config/fish/functions/ggpf.fish
@@ -1,3 +1,3 @@
 function ggpf --wraps "git push" --description 'Git force push into current branch'
-    git push --force-with-lease origin (git branch --show-current) $argv
+    git push --force-with-lease --force-if-includes origin (git branch --show-current) $argv
 end
