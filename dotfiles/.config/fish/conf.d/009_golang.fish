d6556dc69cc192ce36ec39d82d059c9f93811993
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2026-03-12 17:38:08 +0200

    Disable go telemetry via env var

--- dotfiles/.config/fish/conf.d/009_golang.fish
+++ dotfiles/.config/fish/conf.d/009_golang.fish
@@ -2,6 +2,7 @@ if type --query go
     set --export --global GOPATH "$XDG_DATA_HOME/go"
     set --export --global GOBIN "$GOPATH/bin"
     set --export --global GOPROXY direct
+    set --export --global GOTELEMETRY off
 
     fish_add_path $GOBIN
 end
