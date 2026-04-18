872512ac4a8e2909ec61cd2c464403a3cba1d2d1
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2026-04-18 20:38:59 +0300

    Update inline color nvim package

--- dotfiles/.config/nvim/lua/plugins/colorizer.lua
+++ dotfiles/.config/nvim/lua/plugins/colorizer.lua
@@ -1,7 +1,24 @@
 ---@module 'lazy'
 ---@type LazyPluginSpec
 return {
-	"norcalli/nvim-colorizer.lua",
+	"catgoose/nvim-colorizer.lua",
 	event = { "VeryLazy" },
-	config = function() require("colorizer").setup() end,
+	config = function()
+		require("colorizer").setup({
+			options = {
+				display = {
+					mode = "virtualtext",
+					virtualtext = { position = "eol" },
+				},
+				parsers = {
+					tailwind = {
+						enable = false,
+						update_names = false,
+						lsp = { enable = true, disable_document_color = true },
+					},
+				},
+				debounce_ms = 400,
+			},
+		})
+	end,
 }
