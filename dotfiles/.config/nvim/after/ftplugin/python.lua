2649e08f8d43762fae3e391eac543e583adf8bda
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2026-04-18 22:15:48 +0300

    Simplify python config in nvim

--- dotfiles/.config/nvim/after/ftplugin/python.lua
+++ dotfiles/.config/nvim/after/ftplugin/python.lua
@@ -1,24 +1,31 @@
-local o = vim.opt_local
-local cmd = vim.cmd
+---@return boolean
+local function should_use_tabs()
+	local config_files = { "pyproject.toml", "ruff.toml", ".ruff.toml" }
 
--- use pep8 standards
-o.expandtab = true
-o.shiftwidth = 4
-o.tabstop = 4
-o.softtabstop = 4
+	for _, config in ipairs(config_files) do
+		local file = vim.fn.getcwd() .. "/" .. config
 
--- folds based on indentation https://neovim.io/doc/user/fold.html#fold-indent
--- if you are a heavy user of folds, consider using `nvim-ufo`
-o.foldmethod = "indent"
+		if vim.fn.filereadable(file) == 1 then
+			local yq_cmd = string.format(
+				[[yq -r '.tool.ruff.format.indent-style // .format.indent-style // ""' %s 2>/dev/null]],
+				vim.fn.shellescape(file)
+			)
+			local result = vim.fn.trim(vim.fn.system(yq_cmd))
+			if result ~= "" then return result == "tab" end
+		end
+	end
+	return false
+end
 
--- automatically capitalize boolean values. Useful if you come from a
--- different language, and lowercase them out of habit.
-cmd.inoreabbrev("<buffer> true True")
-cmd.inoreabbrev("<buffer> false False")
+vim.opt_local.expandtab = should_use_tabs()
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.softtabstop = 4
 
--- in the same way, we can fix habits regarding comments or None
-cmd.inoreabbrev("<buffer> -- #")
-cmd.inoreabbrev("<buffer> // #")
-cmd.inoreabbrev("<buffer> null None")
-cmd.inoreabbrev("<buffer> none None")
-cmd.inoreabbrev("<buffer> nil None")
+vim.opt_local.foldmethod = "indent"
+
+vim.cmd.inoreabbrev("<buffer> true True")
+vim.cmd.inoreabbrev("<buffer> false False")
+
+vim.cmd.inoreabbrev("<buffer> // #")
+vim.cmd.inoreabbrev("<buffer> none None")
